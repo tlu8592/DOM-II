acdaf52ec6e46bec73d43c3e47ee3d46c4fba606
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-07-11 21:02:52 -0400

    added event listeners: mouseover, mouseout, keydown, and keyup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
 // Your code goes here
+
+// nav links (mouseover and mouse out)
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+navLinks[0].addEventListener('mouseover', event => {
+    event.target.style.border = '4px dotted slategray';
+    event.target.addEventListener('mouseout', event => {
+        event.target.style.border = 'none';
+    });
+})
+
+navLinks[1].addEventListener('mouseover', event => {
+    event.target.style.border = '4px inset #8A9A5B';
+    event.target.addEventListener('mouseout', event => {
+        event.target.style.border = 'none';
+    });
+})
+
+navLinks[2].addEventListener('mouseover', event => {
+    event.target.style.border = '4px dashed sienna';
+    event.target.addEventListener('mouseout', event => {
+        event.target.style.border = 'none';
+    });
+})
+
+navLinks[3].addEventListener('mouseover', event => {
+    event.target.style.border = '4px outset #87CEEB';
+    event.target.addEventListener('mouseout', event => {
+        event.target.style.border = 'none';
+    })
+})
+
+// destinations buttons (keydown)
+const destinationButtons = document.querySelectorAll('.btn');
+
+document.addEventListener('keydown', event => {
+    if (event.key === 's') {
+        destinationButtons[0].style.backgroundColor = '#F4C430';
+        document.addEventListener('keyup', event => {
+            destinationButtons[0].style.backgroundColor = '#17A2B8';
+        })
+    } 
+    if (event.key === 'm') {
+        destinationButtons[1].style.backgroundColor = 'gray';
+        document.addEventListener('keyup', event => {
+            destinationButtons[1].style.backgroundColor = '#17A2B8';
+        })
+    }
+    if (event.key === 'i') {
+        destinationButtons[2].style.backgroundColor = '#9FE2BF';
+        document.addEventListener('keyup', event => {
+            destinationButtons[2].style.backgroundColor = '#17A2B8';
+        })
+    }
+})
+
